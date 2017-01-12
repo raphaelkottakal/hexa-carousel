3276f0482bde69c71c67136192078fab2c492c0e
Raphael Kottakal <raphaelsemail@gmail.com>
2017-01-12 18:44:40 +0530

    change url to /shoes

--- src/data.js
+++ src/data.js
@@ -5,63 +5,63 @@ export default [
       image1: 'http://assets.myntassets.com/assets/images/lookbook/2017/1/11/11484140384831-Armani_1.jpg',
       image2: 'http://assets.myntassets.com/assets/images/lookbook/2017/1/11/11484140384844-Armani_2.jpg',
       image3: 'http://assets.myntassets.com/assets/images/lookbook/2017/1/11/11484140384860-Armani_3.jpg',
-      url1: 'http://www.myntra.com',
-      url2: 'http://www.myntra.com',
-      url3: 'http://www.myntra.com'
+      url1: 'http://www.myntra.com/shoes',,
+      url2: 'http://www.myntra.com/shoes',,
+      url3: 'http://www.myntra.com/shoes',
     },
     {
       title: 'chukka-boots',
       imageUrl: 'http://assets.myntassets.com/v1438783185/Lookgood/Trends/Final/Men/Footwear/CHukka-Boots.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'espadrilles',
       imageUrl: 'http://assets.myntassets.com/v1438783269/Lookgood/Trends/Final/Men/Apparel/espadrilles.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'leather-sandals',
       imageUrl: 'http://assets.myntassets.com/v1438784444/Lookgood/Trends/Final/TRENDS-MANDALS-2907_1.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'jutis',
       imageUrl: 'http://assets.myntassets.com/v1438926955/Lookgood/Trends/Final/Men/Apparel/footwear-jutis.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'brogues',
       imageUrl: 'http://assets.myntassets.com/v1439462636/Lookgood/Trends/Final/Men/Footwear/Trends-13th-Aug-brogues.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'loafers',
       imageUrl: 'http://assets.myntassets.com/v1438784443/Lookgood/Trends/Final/Trends-loafers.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'chukka-boots',
       imageUrl: 'http://assets.myntassets.com/v1438783185/Lookgood/Trends/Final/Men/Footwear/CHukka-Boots.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'espadrilles',
       imageUrl: 'http://assets.myntassets.com/v1438783269/Lookgood/Trends/Final/Men/Apparel/espadrilles.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'leather-sandals',
       imageUrl: 'http://assets.myntassets.com/v1438784444/Lookgood/Trends/Final/TRENDS-MANDALS-2907_1.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'jutis',
       imageUrl: 'http://assets.myntassets.com/v1438926955/Lookgood/Trends/Final/Men/Apparel/footwear-jutis.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     },
     {
       title: 'brogues',
       imageUrl: 'http://assets.myntassets.com/v1439462636/Lookgood/Trends/Final/Men/Footwear/Trends-13th-Aug-brogues.jpg',
-      url: 'http://www.myntra.com'
+      url: 'http://www.myntra.com/shoes',
     }
 ];
